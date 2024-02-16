551dd99f98063b0b28148b1035398b177a4ce72f
Romain Canon <romain.hydrocanon@gmail.com>
2024-02-16 15:45:30 +0100

    fix: remove unneeded `WarmUpCompatibility` trait

--- src/Cache/MapperCacheWarmer.php
+++ src/Cache/MapperCacheWarmer.php
@@ -11,12 +11,21 @@ use Symfony\Contracts\Service\ServiceProviderInterface;
 /** @internal */
 final class MapperCacheWarmer implements CacheWarmerInterface
 {
-    use WarmUpCompatibility;
-
     public function __construct(
         private ServiceProviderInterface $classesToWarmup,
         private MapperBuilder $mapperBuilder
     ) {}
+
+    public function warmUp(string $cacheDir, string $buildDir = null): array
+    {
+        $this->mapperBuilder->warmup();
+
+        foreach ($this->classesToWarmup->getProvidedServices() as $class) {
+            $this->mapperBuilder->warmup($class);
+        }
+
+        return [];
+    }
 
     public function isOptional(): bool
     {
