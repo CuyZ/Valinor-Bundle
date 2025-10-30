596c2789bada9ca324796d0ac0f598a53de827ac
Romain Canon <romain.hydrocanon@gmail.com>
2025-10-30 21:59:41 +0100

    fix: automatically register cache for normalizer builder
    It was an oversight during the release of 2.0 where the `MapperBuilder`
    was split out with the `NormalizerBuilder`.

--- src/Configurator/CacheConfigurator.php
+++ src/Configurator/CacheConfigurator.php
@@ -6,9 +6,10 @@ namespace CuyZ\ValinorBundle\Configurator;
 
 use CuyZ\Valinor\Cache\Cache;
 use CuyZ\Valinor\MapperBuilder;
+use CuyZ\Valinor\NormalizerBuilder;
 
 /** @internal */
-final class CacheConfigurator implements MapperBuilderConfigurator
+final class CacheConfigurator implements MapperBuilderConfigurator, NormalizerBuilderConfigurator
 {
     public function __construct(
         /** @var Cache<mixed> */
@@ -16,6 +17,11 @@ final class CacheConfigurator implements MapperBuilderConfigurator
     ) {}
 
     public function configureMapperBuilder(MapperBuilder $builder): MapperBuilder
+    {
+        return $builder->withCache($this->cache);
+    }
+
+    public function configureNormalizerBuilder(NormalizerBuilder $builder): NormalizerBuilder
     {
         return $builder->withCache($this->cache);
     }
